61333cb5425024ddb9e740b65b118d6dbf104635
SandroVolonte <61095194+SandroVolonte@users.noreply.github.com>
2020-07-21 15:37:39 +0200

    Updates

---
+++ precache-manifest.f733e609628f9d04c900649e313e5322.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f15ab36a06738a8238cb8dfed0e572e",
+    "url": "/quotemachine/index.html"
+  },
+  {
+    "revision": "339b949de6e50532286a",
+    "url": "/quotemachine/static/css/main.00abaddd.chunk.css"
+  },
+  {
+    "revision": "7870e979b7fbf470f9c9",
+    "url": "/quotemachine/static/js/2.bd156435.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quotemachine/static/js/2.bd156435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "339b949de6e50532286a",
+    "url": "/quotemachine/static/js/main.056d4047.chunk.js"
+  },
+  {
+    "revision": "38e6101765f4c875162e",
+    "url": "/quotemachine/static/js/runtime-main.42dbe5df.js"
+  }
+]);
